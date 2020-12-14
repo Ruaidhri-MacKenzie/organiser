89250c72644c9d992935f1ebdaabc5a35c6b0995
Fankadore <fankadore@protonmail.com>
2020-12-14 23:30:45 +0000

    Added delete button

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,15 +1,16 @@
-import React, { useState } from 'react';
-import './Task.css';
+import React, { useState } from "react";
+import "./Task.css";
 
 const Task = ({ name = "", description = "", isCompleted = false }) => {
 	const [isChecked, setIsChecked] = useState(isCompleted);
-	const handleChecked = e => setIsChecked(!isChecked);
+	const handleChecked = (e) => setIsChecked(!isChecked);
 
 	return (
 		<li className="task">
 			<input className="task__checkbox" type="checkbox" checked={isChecked} onChange={handleChecked} />
-			<h1 className="task__name">{name}</h1>
+			<h2 className="task__name">{name}</h2>
 			<p className="task__description">{description}</p>
+			<div className="task__delete">X</div>
 		</li>
 	);
 };
