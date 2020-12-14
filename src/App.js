eeb64c09b3ad8285d256fa3c53cd9be2b80bde0e
Fankadore <fankadore@protonmail.com>
2020-12-14 23:32:02 +0000

    Moved tasks data to TaskList

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import './App.css';
-import TaskList from './components/TaskList/TaskList';
+import "./App.css";
+import TaskList from "./components/TaskList/TaskList";
 
 const App = () => {
-	const tasks = [
-		{ name: "Test", description: "testing", isCompleted: false },
-		{ name: "Testing", description: "testing again", isCompleted: true },
-	];
-	
-  return (
-    <div className="App">
-			Ruaidhri MacKenzie
-			<TaskList tasks={tasks} />
-    </div>
-  );
+	return (
+		<div className="App">
+			<TaskList />
+		</div>
+	);
 };
 
 export default App;
--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,19 +1,31 @@
-import React from 'react';
-import Task from '../Task/Task';
-import './TaskList.css';
+import React, { useState, useEffect } from "react";
+import Task from "../Task/Task";
+import "./TaskList.css";
 
-const TaskList = ({ tasks }) => {
+const TaskList = () => {
+	const [tasks, setTasks] = useState([]);
+
+	useEffect(() => {
+		// Fetch from API with HTTP GET request
+		const tasks = [
+			{ name: "Test", description: "testing", isCompleted: false },
+			{ name: "Testing", description: "testing again", isCompleted: true },
+		];
+
+		setTasks(tasks);
+	}, []);
+
 	const renderTasks = () => {
 		if (tasks == null || tasks.length === 0) {
-			return <p className="task-list__empty">No tasks to display.</p>
-		}
-		else {
+			return <p className="task-list__empty">No tasks to display.</p>;
+		} else {
 			return tasks.map((task, index) => <Task key={task.name + index} name={task.name} description={task.description} isCompleted={task.isCompleted} />);
 		}
 	};
 
 	return (
 		<ul className="task-list">
+			<li className="task-list__add">Add Task</li>
 			{renderTasks()}
 		</ul>
 	);
